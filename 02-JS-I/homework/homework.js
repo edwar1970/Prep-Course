d897708f60ab0cc2cf7c60ede86c743c4f68a1ae
Edwar Salazar <edwar.salazar.p@gmail.com>
2022-11-09 00:10:34 -0300

    four commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -18,7 +18,6 @@ const nuevaMultiplicacion = 10 * 4 === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 4.2;
 
-
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
@@ -28,20 +27,22 @@ const nuevoModulo = 21 % 5 === 4.2;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  suma = x+y
+  return suma;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  resta =x-y
+  return resta;
 }
 
 function multiplica(x, y) {
